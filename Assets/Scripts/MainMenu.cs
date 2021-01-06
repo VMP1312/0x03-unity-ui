6814c5023de1285180ea104be0249cde4addb618
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-01-05 23:26:43 -0500

    UPLOAD

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    public void PlayMaze()
+    {
+        
+
+        SceneManager.LoadScene("maze");
+    }
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text scoreText;
     private int score = 0;
+    public Text healthText;
     public int health = 5;
+    public Text WinLoseText;
+    public Image WinLoseBG;
     private Rigidbody rb;
+ 
 
     // Start is called before the first frame update.
     void Start()
@@ -32,8 +41,9 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("Pickup"))
         {
             score++;
-            Debug.Log("Score: " + score.ToString());
+            // Debug.Log("Score: " + score.ToString());
             other.gameObject.SetActive(false);
+            SetScoreText();
         }
 
         ///Decrement Health when touch a trap.
@@ -41,23 +51,53 @@ public class PlayerController : MonoBehaviour
         {
             health--;
             other.gameObject.SetActive(true);
-            Debug.Log("Health: " + health.ToString());
+            // Debug.Log("Health: " + health.ToString());
+            SetHealthText();
         }
 
         ///Send Goal reach message.
          if(other.gameObject.CompareTag("Goal"))
         {
             other.gameObject.SetActive(true);
-            Debug.Log("You win!");
+            // Debug.Log("You win!");
+            WinLoseBG.gameObject.SetActive(true);
+            WinLoseText.color = Color.black;
+            WinLoseBG.color = Color.green;
+            WinLoseText.text = "You Win!";
+            StartCoroutine(LoadScene(3));
         }
     }
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        
         if(health == 0)
         {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene(0);
+            // Debug.Log("Game Over!");
+            WinLoseBG.gameObject.SetActive(true);
+            WinLoseBG.color = Color.red;
+            WinLoseText.color = Color.white;
+            WinLoseText.text = "Game Over!";
+            StartCoroutine(LoadScene(3));
         }
+    }
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(0);
     }
 }
